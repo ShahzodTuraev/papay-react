03442a29608c29b454575cef11d4823fb55e1bad
shon <turayevshahzodbek@gmail.com>
2023-11-10 23:39:21 +0900

    feat: standart integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "../css/App.css";
-
-function App() {
-  return <div className="App"></div>;
-}
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+// stack-flexbox;
+const App = () => {
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h1"} gutterBottom>
+            Create React App on TypeScript with Redux
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
+  );
+};
 
 export default App;

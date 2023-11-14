464236fbd1d9b4fde7fc48188c06847c5d325011
shon <turayevshahzodbek@gmail.com>
2023-11-14 14:20:02 +0900

    feat: develop logic of footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../css/App.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -14,6 +15,7 @@ import { HomePage } from "./screens/HomePage";
 import NavbarHome from "./components/header";
 import NavbarRestaurant from "./components/header/restaurant";
 import NavbarOthers from "./components/header/others";
+import Footer from "./components/footer";
 
 const App = () => {
   const [path, setPath] = useState();
@@ -29,31 +31,6 @@ const App = () => {
       ) : (
         <NavbarOthers setPath={setPath} />
       )}
-      {/* <nav>
-          <ul>
-            <li>
-              <Link to="/restaurant">Home</Link>
-            </li>
-            <li>
-              <Link to="/community">CommunityPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">MemberPage</Link>
-            </li>
-            <li>
-              <Link to="/help">Help</Link>
-            </li>
-            <li>
-              <Link to="/login">LoginPage</Link>
-            </li>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-          </ul>
-        </nav> */}
 
       <Switch>
         <Route path="/restaurant">
@@ -78,6 +55,7 @@ const App = () => {
           <HomePage />
         </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 };

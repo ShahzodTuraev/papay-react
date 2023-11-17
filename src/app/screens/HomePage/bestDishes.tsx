ca79a06920f5ee648a0fbb6346c32261281b44fe
shon <turayevshahzodbek@gmail.com>
2023-11-18 01:28:43 +0900

    feat: homepage ads section publishing

--- src/app/screens/HomePage/bestDishes.tsx
+++ src/app/screens/HomePage/bestDishes.tsx
@@ -1,10 +1,41 @@
-import { Container } from "@mui/material";
+import { MonetizationOn } from "@mui/icons-material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
 
 const BestDishes = () => {
   return (
-    <div className="top_restaurant_frame">
-      <Container>BestDishes</Container>
+    <div className="best_dishes_frame">
+      <Container>
+        <Stack alignItems={"center"}>
+          <Box className="category_title">Trenddagi ovqatlar</Box>
+          <Stack flexDirection={"row"} sx={{ mt: "43px" }}>
+            {[1, 2, 3, 4].map(() => {
+              return (
+                <Box className="dish_box">
+                  <Stack className="dish_img">
+                    <div className="dish_sale">Normal size</div>
+                    <div className="view_btn">
+                      Batafsil ko'rish
+                      <img
+                        src="/icons/arrow_right.svg"
+                        alt=""
+                        style={{ marginLeft: "9px" }}
+                      />
+                    </div>
+                  </Stack>
+                  <Stack className="dish_desc">
+                    <span className="dish_title_text">Libanon Oozie</span>
+                    <span className="dish_desc_text">
+                      <MonetizationOn />
+                      11
+                    </span>
+                  </Stack>
+                </Box>
+              );
+            })}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 };

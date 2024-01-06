dcdf3f7013ccaed38479f10145f8d80d334f902a
shon <turayevshahzodbek@gmail.com>
2024-01-06 14:30:31 +0900

    feat: communityPage retrieve tartgetArticles data

--- src/app/screens/CommunityPage/index.tsx
+++ src/app/screens/CommunityPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../../css/community.css";
 import {
   Box,
@@ -13,18 +13,70 @@ import CommunityChats from "./communityChats";
 import { TabContext, TabPanel } from "@mui/lab";
 import TargetArticles from "./targetArticles";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
-
-const targetBoArticles = [1, 2, 3];
+import CommunityApiService from "../../apiServices/communityApiService";
+import { BoArticle, SearchArticleObj } from "../../../types/boArticle";
+// REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { retrieveTargetBoArticles } from "./selector";
+import { setTargetBoArticles } from "./slice";
+// REDUX SLICE
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTargetBoArticles: (data: BoArticle[]) =>
+    dispatch(setTargetBoArticles(data)),
+});
+// REDUX SELECTOR
+const targetBoArticlesRetriever = createSelector(
+  retrieveTargetBoArticles,
+  (targetBoArticles) => ({
+    targetBoArticles,
+  })
+);
 
 const CommunityPage = (props: any) => {
-  /****/
+  /**INSTALIZATIONS**/
+  const { setTargetBoArticles } = actionDispatch(useDispatch());
+  const { targetBoArticles } = useSelector(targetBoArticlesRetriever);
   const [value, setValue] = useState("1");
+  const [searchArticlesObj, setSearchArticlesObj] = useState<SearchArticleObj>({
+    bo_id: "all",
+    page: 1,
+    limit: 5,
+  });
+
+  useEffect(() => {
+    const communityService = new CommunityApiService();
+    communityService
+      .getTargetArticles(searchArticlesObj)
+      .then((data) => {
+        setTargetBoArticles(data);
+      })
+      .catch((err) => console.log(err));
+  }, [searchArticlesObj]);
+
   /**HANDLERS**/
   const handleChange = (event: any, newValue: string) => {
+    switch (newValue) {
+      case "1":
+        searchArticlesObj.bo_id = "all";
+        break;
+      case "2":
+        searchArticlesObj.bo_id = "celebrity";
+        break;
+      case "3":
+        searchArticlesObj.bo_id = "evaluation";
+        break;
+      case "4":
+        searchArticlesObj.bo_id = "story";
+        break;
+    }
+    setSearchArticlesObj({ ...searchArticlesObj });
     setValue(newValue);
   };
   const handlePaginationChange = (event: any, value: number) => {
-    console.log(value);
+    searchArticlesObj.page = value;
+    setSearchArticlesObj({ ...searchArticlesObj });
   };
   return (
     <div className="community_page">
--- src/app/screens/CommunityPage/targetArticles.tsx
+++ src/app/screens/CommunityPage/targetArticles.tsx
@@ -6,17 +6,22 @@ import Favorite from "@mui/icons-material/Favorite";
 import Checkbox from "@mui/material/Checkbox";
 
 import moment from "moment";
+import { BoArticle } from "../../../types/boArticle";
+import { serverApi } from "../../../lib/config";
 
 const TargetArticles = (props: any) => {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  // const {article} = props
   return (
     <Stack>
-      {props.targetBoArticles?.map((article: any, index: string) => {
-        const art_img_url = "/community/famous_boy.jpg";
+      {props.targetBoArticles?.map((article: BoArticle) => {
+        const art_img_url = article?.art_image
+          ? `${serverApi}/${article.art_image}`
+          : "/community/famous_boy.jpg";
         return (
           <Link
             className="all_article_box"
-            sx={{ textDecoration: "none" }}
+            sx={{ textDecoration: "none", height: "180px" }}
             href={``}
           >
             <Box
@@ -35,27 +40,30 @@ const TargetArticles = (props: any) => {
                   width={"35px"}
                   style={{ borderRadius: "50%", backgroundSize: "cover" }}
                 />
-                <span className="all_article_author_user">martin</span>
+                <span className="all_article_author_user">
+                  {article?.member_data.mb_nick}
+                </span>
               </Box>
               <Box
                 sx={{ display: "flex", mt: "15px" }}
                 flexDirection={"column"}
               >
-                <span className="all_article_title">evalution</span>
-                <p className="all_article_desc">
-                  Texas De Brazil zor restaurnat!
-                </p>
+                <span className="all_article_title">{article?.bo_id}</span>
+                <p className="all_article_desc">{article?.art_subject}</p>
 
                 <Box className="article_bott_box">
-                  <span>{moment().format("YY-MM-DD HH:MM")}</span>
+                  <span>
+                    {moment(article.createdAt).format("YY-MM-DD HH:MM")}
+                  </span>
                   <Checkbox
                     {...label}
                     icon={<FavoriteBorder />}
                     checkedIcon={<Favorite />}
+                    id={article?._id}
                   />
-                  <span>98 ta</span>
+                  <span>{article?.art_likes}</span>
                   <RemoveRedEyeIcon sx={{ m: "0 10px" }} />
-                  <span>1000 ta</span>
+                  <span>{article?.art_views}</span>
                 </Box>
               </Box>
             </Box>

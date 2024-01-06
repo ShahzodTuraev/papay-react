28c23ccf0629204c44e3f5a7ee6c0420335ffb0e
shon <turayevshahzodbek@gmail.com>
2024-01-06 16:18:35 +0900

    feat: MemberPage develop redux configurations

--- src/types/follow.ts
+++ src/types/follow.ts
@@ -1,6 +1,27 @@
+import { Member } from "./user";
+
 export interface MeFollowed {
   _id: string;
   follow_id: string;
   subscriber_id: string;
   my_following: boolean;
+}
+
+export interface Follower {
+  _id: string;
+  follow_id: string;
+  subscriber_id: string;
+  createdAt: Date;
+  updateAt: Date;
+  subscriber_mb_data: Member;
+  me_followed?: MeFollowed[] | null;
+}
+
+export interface Following {
+  _id: string;
+  follow_id: string;
+  subscriber_id: string;
+  createdAt: Date;
+  updateAt: Date;
+  follow_mb_data: Member;
 }

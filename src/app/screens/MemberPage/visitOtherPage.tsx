3381dece6171bdd8dbeac8ad521ba326d068ac08
shon <turayevshahzodbek@gmail.com>
2024-01-09 18:39:25 +0900

    feat: MemberPage others posts and follows develop

--- src/app/screens/MemberPage/visitOtherPage.tsx
+++ src/app/screens/MemberPage/visitOtherPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -37,7 +37,17 @@ import {
   setChosenSingleBoArticle,
 } from "./slice";
 import { Member } from "../../../types/user";
-import { BoArticle } from "../../../types/boArticle";
+import { BoArticle, SearchMemberArticlesObj } from "../../../types/boArticle";
+import { useHistory } from "react-router-dom";
+import CommunityApiService from "../../apiServices/communityApiService";
+import MemberApiService from "../../apiServices/memberApiService";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../lib/sweetAlert";
+import assert from "assert";
+import { Definer } from "../../../lib/Definer";
+import FollowApiService from "../../apiServices/followApiService";
 // REDUX SLICE
 const actionDispatch = (dispatch: Dispatch) => ({
   setChosenMember: (data: Member) => dispatch(setChosenMember(data)),
@@ -68,7 +78,11 @@ const chosenMemberBoArticleRetriever = createSelector(
 
 const VisitOtherPage = (props: any) => {
   /** INITIALIZINGS **/
-  const [value, setValue] = useState("4");
+  const [articleRebuild, setArticleRebuild] = useState<Date>(new Date());
+  const [followRebuild, setFollowRebuild] = useState<Date>(new Date());
+  const { verifiedMemberData, chosen_mb_id, chosen_art_id } = props;
+  const history = useHistory();
+  const [value, setValue] = useState("2");
 
   const {
     setChosenMember,
@@ -79,10 +93,91 @@ const VisitOtherPage = (props: any) => {
   const { chosenMemberBoArticles } = useSelector(
     chosenMemberBoArticleRetriever
   );
+  const [memberArticleSearchObj, seMemberArticleSearchObj] =
+    useState<SearchMemberArticlesObj>({
+      mb_id: chosen_mb_id,
+      page: 1,
+      limit: 5,
+    });
   const { chosenSingleBoArticle } = useSelector(chosenSingleBoArticleRetriever);
+  useEffect(() => {
+    if (chosen_mb_id === verifiedMemberData?._id) {
+      history.push("/member-page");
+    }
+    const communityService = new CommunityApiService();
+    if (chosen_art_id) {
+      communityService
+        .getChosenArticle(chosen_art_id)
+        .then((data) => {
+          setChosenSingleBoArticle(data);
+          setValue("4");
+        })
+        .catch((err) => console.log(err));
+    }
+    communityService
+      .getMeberCommunityArticles(memberArticleSearchObj)
+      .then((data) => setChosenMemberBoArticles(data))
+      .catch((err) => console.log(err));
+  }, [memberArticleSearchObj, chosen_mb_id, articleRebuild]);
+  useEffect(() => {
+    if (chosen_mb_id === verifiedMemberData?._id) {
+      history.push("/member-page");
+    }
+    const memberService = new MemberApiService();
+
+    memberService
+      .getChosenMember(memberArticleSearchObj.mb_id)
+      .then((data) => setChosenMember(data))
+      .catch((err) => console.log(err));
+  }, [verifiedMemberData, chosen_mb_id, followRebuild]);
   /** HANDLERS **/
   const handlerChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
+  };
+  const renderChosenArticleHandler = async (art_id: string) => {
+    try {
+      const communityService = new CommunityApiService();
+      communityService
+        .getChosenArticle(art_id)
+        .then((data) => {
+          setChosenSingleBoArticle(data);
+          setValue("4");
+        })
+        .catch((err) => console.log(err));
+    } catch (err: any) {
+      console.log(err);
+      sweetErrorHandling(err).then();
+    }
+  };
+  const handlePaginationChange = (event: any, value: number) => {
+    memberArticleSearchObj.page = value;
+    seMemberArticleSearchObj({ ...memberArticleSearchObj });
+  };
+
+  const subscribeHandler = async (e: any) => {
+    try {
+      assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
+      const followService = new FollowApiService();
+      await followService.subscribe(e.target.value);
+      await sweetTopSmallSuccessAlert("subscribed successfully", 700, false);
+      setFollowRebuild(new Date());
+    } catch (err: any) {
+      console.log(err);
+      sweetErrorHandling(err).then();
+    }
+  };
+
+  const unsubscribeHandler = async (e: any) => {
+    try {
+      assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
+      const followService = new FollowApiService();
+      await followService.unsubscribe(e.target.value);
+      await sweetTopSmallSuccessAlert("unsubscribed successfully", 700, false);
+      setFollowRebuild(new Date());
+    } catch (err: any) {
+      console.log(err);
+      sweetErrorHandling(err).then();
+    }
   };
   return (
     <div className="my_page">
@@ -94,7 +189,11 @@ const VisitOtherPage = (props: any) => {
                 <TabPanel value="1">
                   <Box className="menu_name">Maqolalar</Box>
                   <Box className="menu_content">
-                    <MemberPosts />
+                    <MemberPosts
+                      setArticleRebuild={setArticleRebuild}
+                      chosenMemberBoArticles={chosenMemberBoArticles}
+                      renderChosenArticleHandler={renderChosenArticleHandler}
+                    />
                     <Stack
                       sx={{ my: "40px" }}
                       direction={"row"}
@@ -103,8 +202,12 @@ const VisitOtherPage = (props: any) => {
                     >
                       <Box className="bottom_box">
                         <Pagination
-                          count={3}
-                          page={1}
+                          count={
+                            memberArticleSearchObj.page >= 3
+                              ? memberArticleSearchObj.page + 1
+                              : 3
+                          }
+                          page={memberArticleSearchObj.page}
                           renderItem={(item) => (
                             <PaginationItem
                               components={{
@@ -115,6 +218,7 @@ const VisitOtherPage = (props: any) => {
                               color="secondary"
                             />
                           )}
+                          onChange={handlePaginationChange}
                         />
                       </Box>
                     </Stack>
@@ -123,20 +227,30 @@ const VisitOtherPage = (props: any) => {
                 <TabPanel value="2">
                   <Box className="menu_name">Followers</Box>
                   <Box className="menu_content">
-                    <MemberFollowers actions_enabled={false} />
+                    <MemberFollowers
+                      actions_enabled={false}
+                      mb_id={chosen_mb_id}
+                      setFollowRebuild={setFollowRebuild}
+                      followRebuild={followRebuild}
+                    />
                   </Box>
                 </TabPanel>
                 <TabPanel value="3">
                   <Box className="menu_name">Following</Box>
                   <Box className="write_content">
-                    <MemberFollowing actions_enabled={false} />
+                    <MemberFollowing
+                      actions_enabled={false}
+                      mb_id={chosen_mb_id}
+                      setFollowRebuild={setFollowRebuild}
+                      followRebuild={followRebuild}
+                    />
                   </Box>
                 </TabPanel>
 
                 <TabPanel value="4">
                   <Box className="menu_name">Tanlangan Maqola</Box>
                   <Box className="menu_content">
-                    <TViewer text={`<h1>visit other page hi tviewer</h1>`} />
+                    <TViewer chosenSingleBoArticle={chosenSingleBoArticle} />
                   </Box>
                 </TabPanel>
               </Box>
@@ -151,8 +265,8 @@ const VisitOtherPage = (props: any) => {
                     className="user_corner_icon"
                   />
                 </Box>
-                <p className="user_name">Angelina Cute</p>
-                <p className="user_type">Foydalanuvchi</p>
+                <p className="user_name">{chosenMember?.mb_nick}</p>
+                <p className="user_type">{chosenMember?.mb_type}</p>
                 <Box className="social_wrap">
                   <Facebook className="social_icons" />
                   <Instagram className="social_icons" />
@@ -160,16 +274,35 @@ const VisitOtherPage = (props: any) => {
                   <Telegram className="social_icons" />
                 </Box>
                 <Box className="follow_status">
-                  <span style={{ marginRight: "20px" }}>Follower: 3</span>
-                  <span>Followings: 7</span>
+                  <span style={{ marginRight: "20px" }}>
+                    Follower: {chosenMember?.mb_subscriber_cnt}
+                  </span>
+                  <span>Followings: {chosenMember?.mb_follow_cnt}</span>
                 </Box>
-                <p className="user_desc">Salom mening ismim Angelina</p>
-                <Button
-                  style={{ background: "rgba(247, 9, 9, 0.72)" }}
-                  variant="contained"
-                >
-                  Bekor qilish
-                </Button>
+                <p className="user_desc">
+                  {chosenMember?.mb_description ??
+                    "qo'shimcha ma'lumot kiritilmagan"}
+                </p>
+                {chosenMember?.me_followed &&
+                chosenMember.me_followed[0]?.my_following ? (
+                  <Button
+                    style={{ background: "rgba(247, 9, 9, 0.72)" }}
+                    variant="contained"
+                    value={chosenMember?._id}
+                    onClick={unsubscribeHandler}
+                  >
+                    Bekor qilish
+                  </Button>
+                ) : (
+                  <Button
+                    value={chosenMember?._id}
+                    onClick={subscribeHandler}
+                    style={{ background: "#30945e" }}
+                    variant="contained"
+                  >
+                    Follow qilish
+                  </Button>
+                )}
               </Box>
               <Box className="menu_wrapper">
                 <Box onClick={() => setValue("1")} className="tab_menu">
